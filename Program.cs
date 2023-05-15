f82a931264703f7ecb7a531c8bc0665f146bdd43
Kingkarn <Kingkarn.jant@mail.kmutt.ac.th>
2023-05-15 15:31:33 +0700

    save1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        float Vmax = float.Parse(Console.ReadLine()); //Vmax คือความจุของถังน้ำ 
+        float Vdrink = float.Parse(Console.ReadLine()); //Vdrink คือ ปริมาตรน้ำเฉลี่ยที่ผู้เข้าร่วมโครงการดื่มในแต่ละช่วงพัก
+        float Vfill = float.Parse(Console.ReadLine()); //Vfill คือ  ปริมาตรน้ำที่เติมได้ในแต่ละรอบเติมน้ำ
+        int tdrink = int.Parse(Console.ReadLine()); //tdrink คือ ระยะเวลาคั่นระหว่างช่วงพัก
+        int tfill = int.Parse(Console.ReadLine()); //tfill คือ ระยะเวลาคั่นระหว่างรอบเติมน้ำ
+        int tday = int.Parse(Console.ReadLine()); //tday คือ ระยะเวลารวมของกิจกรรมตั้งแต่เริ่มจนจบวัน
+    }
+}
